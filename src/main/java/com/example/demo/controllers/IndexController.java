27d85c9751d6562c6c8f6867f8ece91c215a04d4
Mpholo Leboea <mpholo.leboea@gmail.com>
2019-09-08 07:26:45 +0200

    first commit

---
+++ src/main/java/com/example/demo/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping({"/","index","index/html"})
+	public String index(Model model) {
+		model.addAttribute("message","Hello World");
+		return "index";
+	}
+}
